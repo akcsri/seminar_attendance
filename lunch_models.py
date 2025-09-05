66bc0281b30023c23e4945308000ac28093ca786
金子 明彦 <akihiko.kaneko@csri-japan.com>
2025-09-05 13:17:51 +0900

    u

--- lunch_models.py
+++ lunch_models.py
@@ -16,3 +16,4 @@ class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price_excl_tax = db.Column(db.Numeric(10, 2), nullable=False)
+    image_url = db.Column(db.String(255))
